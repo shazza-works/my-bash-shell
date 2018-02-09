f7a117c4952798c9ca4ee2b85e904497ecab50ba
shazza-works <ogormanad@googlemail.com>
2018-02-09 22:21:48 +0000

    Update-Fri Feb  9 22:21:48 GMT 2018

--- tools/git-update.sh
+++ tools/git-update.sh
@@ -3,16 +3,17 @@
 # Shazza-Works						#
 # Update Script To Push My Git Files			#
 ########################################################
-export home=$HOME
-export bash=$BASH
+export home="$HOME"
+export bash="$BASH"
+export gits="$home/GIT-HUB"
 clear
-ls .
+ls "$gits/"
 read -p "What Git Are You Updating; " -r thegit
 read -p "What Branch Do You Want To Use [Def=master]; " -r branch
 echo -e "\n $branch Selected... \n"
-git status ; sleep 2
-git add . ; wait
-git commit -m "Update-$(date)"
-git push https://github.com/shazza-works/$thegit $branch ; wait
+git status ; sleep 3
+wait ; git add .
+wait ; git commit -m "Update-$(date)"
+wait ; git push https://github.com/shazza-works/$thegit $branch
 git status
 echo -e "\n All Tasks Done + Uploaded \n"
