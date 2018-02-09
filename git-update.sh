518ae91a85bec533af716f6db213dfd6c858e01f
shazza-works <ogormanad@googlemail.com>
2018-02-09 23:04:11 +0000

    Update-Fri Feb  9 23:04:10 GMT 2018

--- git-update.sh
+++ git-update.sh
@@ -3,17 +3,23 @@
 # Shazza-Works						#
 # Update Script To Push My Git Files			#
 ########################################################
-export home="$HOME"
-export bash="$BASH"
-export gits="$home/GIT-HUB"
+export home=$HOME
+export bash=$BASH
+export gits=$home/GIT-HUB/
 clear
-ls "$gits/"
-read -p "What Git Are You Updating; " -r thegit
+#=======================================================#
+printf "\n What Git Are You Updating? \n"
+select file in $gits/* ; do
+test -n "$file" && break; echo ">>> Invalid Selection";
+git=$fine
+done
+#=======================================================#
 read -p "What Branch Do You Want To Use [Def=master]; " -r branch
 echo -e "\n $branch Selected... \n"
 git status ; sleep 3
 wait ; git add .
 wait ; git commit -m "Update-$(date)"
-wait ; git push https://github.com/shazza-works/$thegit $branch
-git status
-echo -e "\n All Tasks Done + Uploaded \n"
+wait ; git push
+https://github.com/shazza-works/$git $branch git status echo -e "\n All Tasks Done + Uploaded \n"
+
+echo -e "\n\n\n\n\t Thanks For Using Another Shazza Tool; \n"
---
+++ tools/make-dirs-secect.sh
@@ -0,0 +1,3 @@
+printf "Please select folder:\n"
+select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
+cd "$d" && pwd
