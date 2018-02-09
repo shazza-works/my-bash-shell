f8e20605bc8f20756c3e33e7316d43e19493723f
shazza-works <ogormanad@googlemail.com>
2018-02-09 23:35:50 +0000

    Update-Fri Feb  9 23:35:50 GMT 2018

--- git-update.sh
+++ git-update.sh
@@ -3,23 +3,21 @@
 # Shazza-Works						#
 # Update Script To Push My Git Files			#
 ########################################################
-export home="$HOME"
-export bash="$BASH"
-export gits="$home/GIT-HUB/"
+export home=$HOME
+export gits=$home/GIT-HUB/
 clear
 #=======================================================#
 printf "\n What Git Are You Updating? \n"
-ls .
-select gitname in $gits/* ; do
-test -n "$gitname" && break; echo ">>> Invalid Selection";
+select file in $gits/* ; do
+test -n "$file" && break; echo ">>> Invalid Selection";
 done
 #=======================================================#
 read -p "What Branch Do You Want To Use [Def=master]; " -r branch
 echo -e "\n $branch Selected... \n"
 git status ; sleep 3
-wait ; git add .
-wait ; git commit -m "Update-$(date)"
-wait ; git push https://github.com/shazza-works/$gitname $branch
+git add .
+git commit -m "Update-$(date)"
+git push https://github.com/shazza-works/$file $branch
 git status
 echo -e "\n All Tasks Done + Uploaded \n"
 echo -e "\n\n\n\n\t Thanks For Using Another Shazza Tool; \n"
