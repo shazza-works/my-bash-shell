3da98315128e908ff55c61c9f839b3ab33f8470a
shazza-works <ogormanad@googlemail.com>
2018-02-09 23:44:12 +0000

    Update-Fri Feb  9 23:44:12 GMT 2018

--- git-update.sh
+++ git-update.sh
@@ -6,12 +6,10 @@
 export home=$HOME
 export gits=$home/GIT-HUB/
 clear
-#=======================================================#
 printf "\n What Git Are You Updating? \n"
 select file in $gits/* ; do
 test -n "$file" && break; echo ">>> Invalid Selection";
 done
-#=======================================================#
 read -p "What Branch Do You Want To Use [Def=master]; " -r branch
 echo -e "\n $branch Selected... \n"
 git status ; sleep 3
