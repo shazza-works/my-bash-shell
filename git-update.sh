40e2a3f2703e55e1e52eb7774fd29a05d056d3a2
shazza-works <ogormanad@googlemail.com>
2018-02-10 00:40:22 +0000

    Update-Sat Feb 10 00:40:22 GMT 2018

--- git-update.sh
+++ git-update.sh
@@ -5,7 +5,8 @@
 # Update Script To Push My Git Files			#
 #########################################################
 export home=$HOME
-export gits=~/GIT-HUB/
+export gits=$home/GIT-HUB/
+filename=$($gits "fullfile")
 clear
 echo "PWD IS $pwd"
 printf "\n What Git Are You Updating? \n"
@@ -17,7 +18,17 @@ echo -e "\n $branch Selected... \n"
 git status ; sleep 3
 wait ; git add .
 wait ; git commit -m "Update-$(date)"
-wait ; git push https://github.com/shazza-works/"$gits $branch"
+wait ; git push https://github.com/shazza-works/$gits $branch
 git status
 echo -e "\n All Tasks Done + Uploaded \n"
 echo -e "\n\n\n\n\t Thanks For Using Another Shazza Tool; \n"
+
+
+
+First, get file name without the path:
+filename=$(basename "$fullfile")
+extension="${filename##*.}"
+filename="${filename%.*}"
+Alternatively, you can focus on the last '/' of the path instead of the '.'
+which should work even if you have unpredictable file extensions:
+filename="${fullfile##*/}"
