88eff7e07c702e83661ce2eb107c0a08c7d9c0dc
shazza-works <ogormanad@googlemail.com>
2018-02-10 08:54:07 +0000

    Update-Sat Feb 10 08:54:07 GMT 2018

--- git-update.sh
+++ git-update.sh
@@ -20,13 +20,3 @@ wait ; git commit -m "Update-$(date)"
 wait ; git push https://github.com/shazza-works/$github $branch
 echo -e "\n All Tasks Done + Uploaded \n"
 echo -e "\n\n\n\n\t Thanks For Using Another Shazza Tool; \n"
-
-
-
-#First, get file name without the path:
-#filename=$(basename "$fullfile")
-#extension="${filename##*.}"
-#filename="${filename%.*}"
-#Alternatively, you can focus on the last '/' of the path instead of the '.'
-#which should work even if you have unpredictable file extensions:
-#filename="${fullfile##*/}"
--- tools/git-update.sh
+++ tools/git-update.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
+#!/data/data/com.termux/files/usr/bin/env bash
 #########################################################
 # Shazza-Works						#
 # Update Script To Push My Git Files			#
-########################################################
+#########################################################
 export home=$HOME
-export gits=$home/GIT-HUB/
 clear
-#=======================================================#
+echo "PWD IS $pwd"
 printf "\n What Git Are You Updating? \n"
-select file in $gits/* ; do
-test -n "$file" && break; echo ">>> Invalid Selection";
+select file in $HOME/GIT-HUB/* ; do
+test -n "$file" && break ; echo ">>> Invalid Selection";
 done
-#=======================================================#
+github=$(basename "$file")
 read -p "What Branch Do You Want To Use [Def=master]; " -r branch
 echo -e "\n $branch Selected... \n"
 git status ; sleep 3
-git add .
-git commit -m "Update-$(date)"
-git push https://github.com/shazza-works/$file $branch
-git status
+wait ; git add .
+wait ; git commit -m "Update-$(date)"
+wait ; git push https://github.com/shazza-works/$github $branch
 echo -e "\n All Tasks Done + Uploaded \n"
 echo -e "\n\n\n\n\t Thanks For Using Another Shazza Tool; \n"
