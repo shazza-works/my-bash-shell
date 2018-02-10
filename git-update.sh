164c4fa02f2d5c0538970d42c85fc562ffcdf612
shazza-works <ogormanad@googlemail.com>
2018-02-10 00:01:39 +0000

    Update-Sat Feb 10 00:01:39 GMT 2018

--- git-update.sh
+++ git-update.sh
@@ -3,8 +3,7 @@
 # Shazza-Works						#
 # Update Script To Push My Git Files			#
 ########################################################
-export home=$HOME
-export gits=$home/GIT-HUB/
+export gits=$HOME/GIT-HUB/
 clear
 printf "\n What Git Are You Updating? \n"
 select file in $gits/* ; do
@@ -12,6 +11,7 @@ test -n "$file" && break; echo ">>> Invalid Selection";
 done
 read -p "What Branch Do You Want To Use [Def=master]; " -r branch
 echo -e "\n $branch Selected... \n"
+echo $pwd
 git status ; sleep 3
 git add .
 git commit -m "Update-$(date)"
