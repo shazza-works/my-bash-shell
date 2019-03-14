47ca03daee416fadd2023da5f8b043067fbc93fb
Shazza-Works <ogormanad@googlemail.com>
2019-03-14 23:55:08 +0000

    Changed_On_Thu Mar 14 23:55:08 GMT 2019

--- tools/Start_Postgress.sh
+++ tools/Start_Postgress.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/env bash   #SET TO EXPORT ALL
 ##################################################################
 # Shazza-Works 9/02/18                                           #
-# Stript to setup start postgresql on Termix.                    #
+# Stript to setup start postgresql on Termix.               V2   #
 ##################################################################
 export home=$HOME		  # Set home
 export PGDATA=$home/database	  # Set postgress working dir
@@ -9,52 +9,68 @@ def_db=$home/database		  # db-files
 name=`whoami` 			  # Me!
 clear ; ls			  # Clear + List dir
 # Maybe you want to set your own?
-while true; do
+function check_files {
 	if [ ! -d "$def_db" ]; then
 		echo -e "\n"
-		read -p "You Will Need To Make Files? [Y/y]" -n 1 -r create
-		echo -e "\n"
-		if [[ $create =~ ^[Yy]$ ]]; then
-			mkdir $home/database
-			echo -e "\n DB Made @"$def_db" \n"
-		fi
+		cf='888'
 	else
-		echo -e "\n One Already There, Will Clean!\n "
-		echo -e "\n"
-		read -p " Clear home/DB! SURE? [Y/N] " -n 1 -r damsure
-		echo -e "\n"
-		if [[ $damsure =~ ^[Yy]$ ]]; then
-			echo -e "\n Will Wipe Old Db_Files... \n"
-			sleep 5
-			rm -rf $home/database/*
-			echo -e "\n File's Cleaned \n"
-			initdb /$def_db ; wait
-			echo -e "\n Please Wait 5 Sec...\n " ; sleep 5
-			pg_ctl -D /$def_db -l $def_db/db_logfile start ; wait
-			echo -e "\n The Server Info..."
-			grep "address" $home'/database/db_logfile' | grep "port"
- 			echo -e $name"@address:port \n"
-			break
-		else
-			break
-		fi
+		cf='0'
 	fi
-done
-#----------------------shutdown the service---------------------#
+}
+
+function create_new {
+	read -p "You Will Need To Make Files? [Y/y]" -n 1 -r create
+	if [[ $create =~ ^[Yy]$ ]]; then
+		mkdir $home/database
+		echo -e "\n DB Made @"$def_db" \n"
+	else
+		echo -e "\n Files --Left--"
+		echo -e "\n Going to Exit\n"
+		exit
+	fi
+}
+
+function wipe_old {
+	echo -e "\n"
+	read -p " Clear home/DB! SURE? [Y/N] " -n 1 -r damsure
+	echo -e "\n"
+	if [[ $damsure =~ ^[Yy]$ ]]; then
+		echo -e "\n Will Wipe Old Db_Files... \n"
+		sleep 2
+		rm -rf $home/database/*
+		echo -e "\n File's Cleaned \n"
+	fi
+}
+
+function start_server {
+	initdb /$def_db ; wait
+	pg_ctl -D /$def_db -l $def_db/db_logfile start ; wait
+	echo -e "\n The Server Info..."
+}
+
+function stop_server {
+	pg_ctl -D $def_db'/' -m fast stop ; wait
+}
+
+check_files
+if [[ $cf -eq '888' ]]; then
+	create_new ; wait
+elif [[ $cf -eq '0' ]]; then
+	wipe_old ; wait
+fi
+start_server ; wait
+
 while true; do
-	echo -e "\n"
-	read -p  "When you are ready... [Qq]=Kill [Xx]=Quit" -n 1 -r quitty
-	echo -e "\n"
-	if [[ $quitty =~ ^[Qq]$ ]]; then
-		pg_ctl -m fast stop		# No Cleanup As May Need Files.
-		wait				# And Will Reset Next Time.
-		break
-	elif [[ $quitty =~ ^[Xx]$ ]]; then
+	read -p  "When you are ready... [Qq]=Kill-Server [Xx]=Quit-Exit> " -n 1 -r quitty
+	if [[ $quitty =~ ^[Xx]$ ]]; then
 		echo -e "\n You Are Quitting With Db Running...\n "
-		echo -e " You Can Stop With [pg_ctl -m fast stop] "
+		echo -e " You Can Stop With [pg_ctl -m fast stop]\n"
+		break
+	elif [[ $quitty =~ ^[Qq]$ ]]; then
+		stop_server ; wait
 		break
 	fi
+
 done
-clear ; # Clear
-# Print Hoyahh
 echo -e "\n\n\n\n\tTHANK-YOU FOR USING A SHAZZA TOOL...\n\n\n\n"
+exit
