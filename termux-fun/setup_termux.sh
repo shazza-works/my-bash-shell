d5ecf77757c8fad6779b51d93566e3e27230137c
Shazza-Works <ogormanad@googlemail.com>
2019-03-15 02:50:50 +0000

    Changed_On_Fri Mar 15 02:50:50 GMT 2019

---
+++ termux-fun/setup_termux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Tool to set up Termux for first use.
+# Shazza-Works
+# 15/March/2019
+clear
+echo -e "\nThis tool will setup some of the more handny tools on termux. \nPlus setup the storage..\n"
+echo "starting in 5 Sec... "; sleep 10
+pkg update --yes ; pkg install termux-api tree python* python-dev jq dnsutils termux-tools mlocate nmap nano git curl lynx wget
+pkg update
+termux-setup-storage
+echo -e "alias cl='clear'\n" > $HOME/.bash_profile
+echo -e "\n YOU CAN NOW CLEAR WITH CL [CHECK .BASH_PROFILE]\n"
+echo -e "screenfetch\n" >> $HOME/.bash_profile
+echo -e "Removing (Nasty) MotD msg\n"
+echo '' > /data/data/com.termux/files/usr/etc/motd
+echo -e "\n\nYou need to Re-Start your termux with Ctl-D \n"
+sleep 3
+exit
