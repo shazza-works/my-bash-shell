00d251b9a5f49779d917fc56741e579a8b3c2a7f
root <root@localhost.localdomain>
2019-06-09 18:23:13 +0100

    Changed_On_Sun  9 Jun 18:23:13 BST 2019

--- bash/spinner.sh
+++ bash/spinner.sh
@@ -1,7 +1,19 @@
 i=1
-sp="/-\|"
+sp="/ ¬ \ |"
 echo -n ' '
-while true
-do
-    printf "\b${sp:i++%${#sp}:1}"
+while true; do
+	printf "\b${sp:i++%${#sp}:1}"
+	sleep 0.24
 done
+
+
+#NEW
+
+#i=1
+#sp="/-\|"
+#echo -n ' '
+#while true
+#do
+#    printf "\b${sp:i++%${#sp}:1}"
+#done
+
