6e2e9d1d5eabf57b3a67f5f950a1aa949447b886
Shazza-Works <ogormanad@googlemail.com>
2019-07-05 14:42:33 +0100

    Changed_On_Fri Jul  5 14:42:33 BST 2019

--- git_update_app/all-git-update.sh
+++ git_update_app/all-git-update.sh
@@ -1,13 +1,14 @@
-function gitalldown() {
+echo $0
+function gitalldown {
 	for i in `ls`; do
-		if [[ ! $i == $PWD ]]; then
+		if [[ ! -f $i ]]; then
 			cd $i
-			git pull
+			echo "$pwd >>> WORKING..."
+			echo -e "\e[1;31;42m$(git pull)\e[0;0;0m"
 			cd - > /dev/null
 		else
-			echo "Left $i Alone !"
+			echo -e "\e[1;33;44mLeft $i Alone !\e[0;0;0m"
 		fi
 	done
 }
-
-gitalldown()
+gitalldown
